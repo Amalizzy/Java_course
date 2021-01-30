b3388d6f28c3346597210c876ae22dfb0346514b
Dragan Zrnic <dragan.zrnic@blc.edu.ba>
2021-01-30 23:25:14 +0100

    Finished lesson 6

--- Java_course/src/chapter6/HomeAreaCalculator.java
+++ Java_course/src/chapter6/HomeAreaCalculator.java
@@ -2,6 +2,17 @@ package chapter6;
 
 public class HomeAreaCalculator {
     public static void main(String[] args) {
+        Rectangle room1=new Rectangle();
+        room1.setLength(5);
+        room1.setWidth(2);
+        double areaOfRoom1= room1.calculateArea();
+
+        Rectangle room2=new Rectangle(30,50);
+        double areaOfRoom2=room2.calculateArea();
+
+        double totalArea=areaOfRoom1+areaOfRoom2;
+
+        System.out.println("Area of both rooms" + totalArea);
 
     }
 }
